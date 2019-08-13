31f5c434b012a414cc1ec4f8d53cadeb69c65f7c
Christa Stephens <christastephens@quickenloans.com>
2019-08-13 14:20:15 -0400

    added proxy variable

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ this is my data contract. it will define
 how my data will be displayed to my users
 It's a model of the API that I am using.
 */
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     let long;
     let lat;
     //if this exist in the browser
@@ -19,6 +19,7 @@ window.addEventListener('load', () => {
                 long = position.coords.longitude;
                 lat = position.coords.latitude;
                 //using interpolation "`` & {}" to insert lat and long
+                const proxy = 'https://cors-anywhere.herokuapp.com/';
                 const api = `https://api.darksky.net/forecast/8428b105f7d1f93347ffb78b2ef9761e/${lat},${long}`;
 
                 fetch(api)
@@ -27,10 +28,8 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     console.log(data);
-                })
-
+                });
             });
-
             //to get information
             //a get request of the api url
             //after it gets the information it carries out the "then" statement.
@@ -38,7 +37,5 @@ window.addEventListener('load', () => {
             //it will come back from the server..can be named whatever. typically named response.
             //the response will have to be converted to json. it can easily be used in your javascript.
             //after it made it into a json, whe can use the "then" statement because this is where the data is.
-
     }
-
 });
